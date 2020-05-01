8b260e15ef48ab59d565463fab089e1a4ef61ce0
Jakub Sałamun <zellanderplx@gmail.com>
2020-05-01 22:12:34 +0200

    asd

--- Projekt_P4_Bookshop/Projekt_P4_Bookshop/MainWindow.xaml.cs
+++ Projekt_P4_Bookshop/Projekt_P4_Bookshop/MainWindow.xaml.cs
@@ -24,9 +24,11 @@ namespace Projekt_P4_Bookshop
     {
         public static string username_id_mw;
         public static string author_name_f;
+        private int order_n = 1;
 
-        
-        
+
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,11 +37,13 @@ namespace Projekt_P4_Bookshop
         private void username_name_TextChanged(object sender, TextChangedEventArgs e)
         {
             username_name.Text = LoginScreen.username.ToUpper();
-          
-
+      
         }
 
-
+        private void txt_username_id_mw_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txt_username_id_mw.Text = LoginScreen.username_id_lw;
+        }
 
         private void button_load_book_Click(object sender, RoutedEventArgs e)
         {
@@ -142,6 +146,7 @@ namespace Projekt_P4_Bookshop
                     string query = "select Author_name as Author,Title_name as Title,Published_year as Published,Retail_price as Price from [dbo].[STOCK] where Author_name=@Author_name";
                     SqlCommand sqlCommand = new SqlCommand(query, sqlConn);
                     sqlCommand.CommandType = CommandType.Text;
+                    
                     sqlCommand.Parameters.AddWithValue("@Author_name", txt_name_authora_find.Text);
 
 
@@ -166,15 +171,35 @@ namespace Projekt_P4_Bookshop
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
             var data = data_grid_table.SelectedItem;
             string author_n = (data_grid_table.SelectedCells[0].Column.GetCellContent(data) as TextBlock).Text;
             txt_author_shop.Text = author_n;
             string title_n = (data_grid_table.SelectedCells[1].Column.GetCellContent(data) as TextBlock).Text;
             txt_title_shop.Text = title_n;
-            string publisher_n = (data_grid_table.SelectedCells[2].Column.GetCellContent(data) as TextBlock).Text;
-            txt_publisher_year.Text = publisher_n;
             string price_n = (data_grid_table.SelectedCells[3].Column.GetCellContent(data) as TextBlock).Text;
             txt_price_shop.Text = price_n;
+
+            string connectionString=@"Data Source=DESKTOP-MPTGS57\SQLEXPRESS;Initial Catalog=BookStore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                username_id_mw= LoginScreen.username_id_lw;
+                sqlConn.Open();
+                SqlCommand sqlCmd = new SqlCommand("CartBookAdd", sqlConn);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@Order_ID", txt_order_number.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Customer_ID",txt_username_id_mw.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Book_name", txt_title_shop.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Price", txt_price_shop.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Amount", txt_numer_book.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Dodano do koszyka!");
+
+            }
+            
+
+
+
 
         }
 
@@ -183,6 +208,27 @@ namespace Projekt_P4_Bookshop
             Cart cart = new Cart();
             cart.Show();
             this.Close();
+
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            order_n++;
+            txt_order_number.Text = order_n.ToString();
+            
+           
+        }
+
+        private void button_back_order_number_Click(object sender, RoutedEventArgs e)
+        {
+            if (order_n>1)
+            {
+                order_n--;
+                txt_order_number.Text = order_n.ToString();
+            }
+            
+        }
+
+ 
     }
 }
